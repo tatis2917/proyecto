014eca24ba4ea554ad2cf939fca123df45aa049b
Tatis <tatiana.arias39089@ucaldas.edu.co>
2023-09-19 20:38:50 -0500

    setup

---
+++ src/test/java/edu/ucaldas/demo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ucaldas.demo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
